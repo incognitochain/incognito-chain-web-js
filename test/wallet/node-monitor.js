1288e21733254a8c0cc3d48a3914b45e8a3feadf
SangNX <sang@incognito.org>
2022-04-15 15:48:42 +0700

    update method test node

---
+++ test/wallet/node-monitor.js
@@ -0,0 +1,42 @@
+const {
+    PRV_ID,
+    ACCESS_ID,
+    PRIVACY_VERSION,
+    setupWallet
+} = require("./constants")
+
+let wallet;
+let accountSender;
+let pDexV3Instance;
+
+async function setup() {
+    const data = await setupWallet();
+    wallet = data.wallet;
+    accountSender = data.accountSender;
+    pDexV3Instance = data.pDexV3Instance;
+}
+
+async function TestGetBalance() {
+    try {
+        const tokenIDs = [
+            PRV_ID,
+        ];
+        let task = tokenIDs.map((tokenID) =>
+            accountSender.getBalance({
+                tokenID,
+                version: PRIVACY_VERSION,
+            })
+        );
+        console.log("BALANCE", await Promise.all(task));
+    } catch (e) {
+        console.log("TestGetBalance error: ", e);
+    }
+}
+
+async function RunTest() {
+    console.log("BEGIN WEB PDEX3 TEST");
+    await setup();
+    await TestGetBalance();
+}
+
+RunTest()
