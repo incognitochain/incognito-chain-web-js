dc042190060e5f48b453f829eb41e7be6e77a199
SangNX <sang@incognito.org>
2022-04-16 01:31:43 +0700

    update method test node

--- test/wallet/node-monitor.js
+++ test/wallet/node-monitor.js
@@ -6,6 +6,7 @@ const {
 } = require("./constants")
 const { PRIVATE_ACCOUNTS } = require("./setup.constants");
 const { getNodeStatus, getNodeReward } = require("./node-service");
+const bn = require("bn.js");
 let senders;
 let authToken;
 
@@ -25,17 +26,22 @@ async function GetNodeRewards() {
                 await getNodeReward(PaymentAddress)
             ])
             console.log("=====> " + name);
+            console.log(status)
             console.log({
                 Status: status.Status,
                 Role: status.Role,
                 NextEventMsg: status.NextEventMsg,
                 IsOldVersion: status.IsOldVersion,
-                Reward: reward / 1e9
+                Reward: reward / 1e9,
+                AutoStake: status.AutoStake,
+                IsSlashed: status.IsSlashed
             })
             return {
                 accountSender,
                 name,
-                reward
+                reward,
+                autoStake: status.AutoStake,
+                isSlashed: status.IsSlashed
             }
         })
         rewards = await Promise.all(tasks)
@@ -44,7 +50,6 @@ async function GetNodeRewards() {
     }
     return rewards
 }
-
 
 async function WithdrawNodeRewards(accounts) {
     const logs = []
@@ -72,42 +77,51 @@ async function WithdrawNodeRewards(accounts) {
 }
 
 async function CreateAndSendStakeNodes(accounts) {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    for (let i = 0; i <= accounts.length - 1; i++) {
+        const account = accounts[i];
+        const { autoStake, name } = account;
+        if (!autoStake) {
+            console.log('Unstaked name ===> ', name)
+        }
+    }
     const logs = []
-    let _input = ''
+    let _input = '';
     readline.question(
         `Enter your node name to restake: \n`,
-        input => {
+        async input => {
+            console.log()
             _input = input;
-        })
-    if (!_input || !_input.trim()) return;
-    _input = _input.trim();
-    try {
-        for (let i = 0; i <= accounts.length - 1; i++) {
-            const account = accounts[i];
-            const { accountSender, reward, name } = account;
+            console.log('_input: ', _input)
+            if (!_input || !_input.trim()) return readline.close();
+            _input = _input.trim();
+            for (let i = 0; i <= accounts.length - 1; i++) {
+                const account = accounts[i];
+                const { accountSender, name } = account;
 
-            // Check stake exact account name
-            if (_input.toLowerCase().includes(name.toLowerCase())) {
-                const prvBalance = await accountSender.getBalance({ tokenID: PRV_ID, version: PRIVACY_VERSION });
-                console.log(prvBalance)
+                // Check stake exact account name
+                if (_input.toLowerCase().includes(name.toLowerCase())) {
+                    const prvBalance = await accountSender.getBalance({ tokenID: PRV_ID, version: PRIVACY_VERSION });
+                    const minBalance = 1750000000100;
+                    if (new bn(prvBalance).gt(new bn(minBalance))) {
+                        console.log(prvBalance)
+                        const tx = await accountSender.createAndSendStakingTx({
+                            transfer: { fee: FeePerTx},
+                            extra: { version: PRIVACY_VERSION },
+                        });
+                        logs.push({
+                            name,
+                            tx
+                        })
+                    }
+                }
             }
-            // console.log({ reward, name })
-            // if (reward > 0) {
-            //     const tx = await accountSender.createAndSendWithdrawRewardTx({
-            //         transfer: { fee: FeePerTx, tokenID: PRV_ID },
-            //         extra: { version: PRIVACY_VERSION },
-            //     });
-            //     logs.push({
-            //         reward,
-            //         name,
-            //         tx
-            //     })
-            // }
-        }
-        console.log(logs);
-    } catch (error) {
-        console.log('WithdrawNodeRewards error: ', error)
-    }
+            console.log(logs);
+            readline.close()
+        })
 }
 
 async function ActionsWithNode(accounts) {
@@ -117,9 +131,14 @@ async function ActionsWithNode(accounts) {
     })
     let fn;
     let params;
-    console.log('\n======================== \n')
+    console.log('\n========================\n')
+    console.log('What do you want to do?');
+    console.log('1: Withdraw all rewards');
+    console.log('2: Stake your node');
+    console.log('3: Cancel')
+    console.log('\n========================\n')
     readline.question(
-        `What do you want to do? \n1: Withdraw all rewards \n2: Stake your node\n3: Cancel\n\n========================\n`,
+        ``,
         action => {
             switch (action) {
                 case '1':
@@ -127,9 +146,9 @@ async function ActionsWithNode(accounts) {
                     params = accounts;
                     break;
                 case '2':
+                    readline.close()
                     fn = CreateAndSendStakeNodes;
                     params = accounts;
-                    console.log('SANG TEST::: 2')
                     break;
                 default:
                     break;
